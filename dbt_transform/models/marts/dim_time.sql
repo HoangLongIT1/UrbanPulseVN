2534f303e01ffe1f86eae6df568aeac2d2ec3907
LongCodeNoJutsu <hoanglong20112005@gmail.com>
2026-05-12 06:27:24 +0700

    revert: restore correct dbt_utils.date_spine syntax with quoted SQL expressions in dim_time.sql

--- dbt_transform/models/marts/dim_time.sql
+++ dbt_transform/models/marts/dim_time.sql
@@ -8,8 +8,8 @@
 with date_spine as (
     {{ dbt_utils.date_spine(
         datepart="day",
-        start_date=cast('2024-01-01' as date),
-        end_date=cast('2027-01-01' as date)
+        start_date="cast('2024-01-01' as date)",
+        end_date="cast('2027-01-01' as date)"
     ) }}
 ),
 
